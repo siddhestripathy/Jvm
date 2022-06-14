efa45c76fe30da62ea288411f6d8079eba5e53e2
siddhestripathy <siddhestripathy@gmail.com>
2022-06-14 17:22:24 +0530

    initial

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Cal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstnum;
+            double secondnum;
+            double answer;
+            string ops;
+            Console.WriteLine("\t\t\tCONSOLE CALCULATOR!\n");
+            Console.WriteLine("\t\t\tEnter the first number:");
+            firstnum = Double.Parse(Console.ReadLine());
+            Console.WriteLine("\t\t\tSelect an Operator:(+,-,*,/,%,^)\n");
+            ops = Console.ReadLine();
+            Console.WriteLine("\t\t\tEnter the second number:");
+            secondnum = Double.Parse(Console.ReadLine());
+            if (ops == "+")
+            {
+                answer = firstnum +  secondnum;
+                Console.Write(answer);
+            }
+            if (ops == "-")
+            {
+                answer = firstnum - secondnum;
+                Console.Write(answer);
+            }
+            if (ops == "*")
+            {
+                answer = firstnum * secondnum;
+                Console.Write(answer);
+            }
+            if (ops == "/")
+            {
+                answer = firstnum / secondnum;
+                Console.Write(answer);
+            }
+            if (ops == "%")
+            {
+                answer = firstnum % secondnum;
+                Console.Write(answer);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
